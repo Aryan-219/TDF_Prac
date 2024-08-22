fb8038352d081f2faf6aa80bc8cdac128155f6ad
Aryan-219 <aryanagr219@gmail.com>
2024-08-22 13:56:08 +0530

    deleted .class

--- WEB-INF/src/utils/SMSSender.java
+++ WEB-INF/src/utils/SMSSender.java
@@ -5,14 +5,14 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SMSSender {
-    static final String ACCOUNT_SID = "AC9a944ee37dbe214bcff11881d473cee3";
-    static final String AUTH_TOKEN = "532bd41a586858ce9935c0efbff0e0ca";
+    static final String ACCOUNT_SID = "";
+    static final String AUTH_TOKEN = "";
 
     public static void sendOTP(String phone, String otp) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
             new com.twilio.type.PhoneNumber("+91"+phone),
-                new com.twilio.type.PhoneNumber("+14432054383"),
+                new com.twilio.type.PhoneNumber(""),
                 "Welcome to TDF! OTP: "+otp)
             .create();
 
